dbc8ef03cb49b35da0503348d00a173598681b0b
Dante Soares <dante.m.soares@rice.edu>
2021-03-09 16:49:36 -0600

    Made the route pattern match more specific and use the match? method instead of match

--- lib/openstax/healthcheck/middleware.rb
+++ lib/openstax/healthcheck/middleware.rb
@@ -1,14 +1,14 @@
 module OpenStax
   module Healthcheck
     class Middleware
-      ROUTE_PATTERN = /\/ping\/?/
+      ROUTE_PATTERN = /\A\/ping\/?\z/
 
       def initialize(app)
         @app = app
       end
 
       def call(env)
-        return [ 200, {}, [] ] if ROUTE_PATTERN.match env['PATH_INFO']
+        return [ 200, {}, [] ] if ROUTE_PATTERN.match? env['PATH_INFO']
 
         @app.call env
       end
